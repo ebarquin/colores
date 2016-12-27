07b067860bdac8b2419f3d66d239fef78f02d1da
Eugenio Barquin <eu.barquin@me.com>
2016-12-27 22:47:41 +0100

    El switch ya tiene funcionalidad. Cuando enciende activa el knob y cambia el color de fondo

--- Colores/ViewController.swift
+++ Colores/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var btnSwitch: UIButton!
 
+    @IBOutlet weak var imgKnobBase: UIImageView!
+    
+    @IBOutlet weak var imgKnob: UIImageView!
+    
+       override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgKnob.isHidden = true
+        imgKnobBase.isHidden = true
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_off"), for: .normal)
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_on"), for: .selected)
+    }
+    
+    @IBAction func btnSwitchPressed(_ sender: UIButton) {
+        btnSwitch.isSelected = !btnSwitch.isSelected
+        if btnSwitch.isSelected {
+            imgKnob.isHidden = false
+            imgKnobBase.isHidden = false
+            view.backgroundColor = UIColor(hue: 0.5, saturation: 0.5, brightness: 0.75, alpha: 1.0)
+        } else {
+            view.backgroundColor = UIColor(hue: 0.5, saturation: 0, brightness: 0.2, alpha: 1.0)
+            imgKnob.isHidden = true
+            imgKnobBase.isHidden = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  
 }
 
